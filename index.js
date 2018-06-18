049480269740b4b86fd759712d287343a54e16e0
Jason Norwood-Young <jason@10layer.com>
2018-06-18 12:01:05 +0200

    Don't exclude articles we've seen - rather run through them all and PUT (update) ones we've seen. Also, browser.close on error.

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ const moment = require("moment");
 	console.log("Loading url", process.env.AFP_URL);
 
 	page.on("response", response => {
+		// console.log("Got response", response.url);
 		(async () => {
 			if (
 				response.url ===
 				"http://www.afpforum.com/AFPForum/AddDateCriterion/mobileapi.ashx"
 			) {
+				// console.log("Response url ", "http://www.afpforum.com/AFPForum/AddDateCriterion/mobileapi.ashx");
 				var jsonResponse = await response.json();
 				var articles = jsonResponse.Data.Results;
 				globalArticles = articles;
 				// console.log(articles);
 				await page.waitFor(5000);
 				for (let article of articles) {
-					let exists = storedArticles.find(
-						storedArticle =>
-							storedArticle.provider_uid === article.UniqueName
-					);
-					if (!exists) {
+
+					// if (!exists) {
+						// console.log({ article });
 						try {
 							// console.log(
 							// 	`Waiting for [data-uniquename='${
@@ -64,30 +64,32 @@ const moment = require("moment");
 						}
 						// console.log("Done with article");
 						await page.waitFor(500);
-					}
+					// }
 				}
 				console.log("All done");
 				await page.waitFor(2000);
 				await browser.close();
+				console.timeEnd("AFP update");
 			} else if (
 				response.url ===
 				"http://www.afpforum.com/AFPForum/DocumentGet/mobileapi.ashx"
 			) {
+				// console.log("Response url", "http://www.afpforum.com/AFPForum/DocumentGet/mobileapi.ashx");
 				var jsonResponse = await response.json();
 				// console.log(jsonResponse);
 				var article = jsonResponse.Data;
 				var globalArticle = globalArticles.find(
 					ga => ga.UniqueName === jsonResponse.ContextData
 				);
-				console.log(jsonResponse.ContextData, article.Title);
+				// console.log(jsonResponse.ContextData, article.Title);
 				var body = article.Description;
-				console.log({ globalArticle });
-				console.log(globalArticle.Date + " " + globalArticle.Time);
+				// console.log({ globalArticle });
+				// console.log(globalArticle.Date + " " + globalArticle.Time);
 				var date = moment(
 					globalArticle.Date + " " + globalArticle.Time,
 					"DD/MM/YYYY HH:mm:ss"
 				).format("YYYY-MM-DDTHH:mm:ss");
-				console.log({ date });
+				// console.log({ date });
 				var data = {
 					headline: article.Title,
 					blurb: "",
@@ -100,67 +102,59 @@ const moment = require("moment");
 					keywords: article.Keywords.split("|"),
 					date
 				};
-				// console.log({ data });
-				await rest.post(process.env.API_ENDPOINT, {
-					data,
-					username: process.env.API_USERNAME,
-					password: process.env.API_PASSWORD
-				});
-				// console.log({ article });
+				let exists = storedArticles.find(
+					storedArticle => storedArticle.provider_uid === data.provider_uid
+				);
+				if (exists) {
+					await rest.put(`${ process.env.API_ENDPOINT }/${ exists._id }`, {
+						data,
+						username: process.env.API_USERNAME,
+						password: process.env.API_PASSWORD
+					});
+				} else {
+					await rest.post(process.env.API_ENDPOINT, {
+						data,
+						username: process.env.API_USERNAME,
+						password: process.env.API_PASSWORD
+					});
+				}
 			}
 		})();
 	});
-	// Load the site
-	await page.goto(process.env.AFP_URL);
-	var storedArticleData = await rest.get(process.env.API_ENDPOINT, {
-		fields: "provider_uid",
-		username: process.env.API_USERNAME,
-		password: process.env.API_PASSWORD
-	});
-	var storedArticles = storedArticleData.data;
+	try {
+		// Load the site
+		console.time("AFP update");
+		await page.goto(process.env.AFP_URL);
+		var storedArticleData = await rest.get(`${ process.env.API_ENDPOINT }?fields=provider_uid`, {
+			username: process.env.API_USERNAME,
+			password: process.env.API_PASSWORD
+		});
+		var storedArticles = storedArticleData.data;
+		// Get rid of blocking page
+		await page.waitForFunction(
+			"document.getElementById('shortcutParent').style.display == 'block'"
+		);
+		await page.waitForFunction(
+			"document.getElementById('shortcutParent').style.display = 'none'"
+		);
 
-	// Get rid of blocking page
-	await page.waitForFunction(
-		"document.getElementById('shortcutParent').style.display == 'block'"
-	);
-	await page.waitForFunction(
-		"document.getElementById('shortcutParent').style.display = 'none'"
-	);
+		// Login
+		await page.waitForSelector("#username");
+		await page.type("#username", process.env.AFP_USERNAME);
+		await page.type("#Password", process.env.AFP_PASSWORD);
+		await page.click("#btnConnexion > input");
 
-	// Login
-	await page.waitForSelector("#username");
-	await page.type("#username", process.env.AFP_USERNAME);
-	await page.type("#Password", process.env.AFP_PASSWORD);
-	await page.click("#btnConnexion > input");
+		await page.waitForSelector("#login");
+		// Accept T&Cs
+		await page.waitForSelector("#cbAcceptCGU");
+		await page.click("#cbAcceptCGU");
+		await page.click("#btnOKCGU");
 
-	await page.waitForSelector("#login");
-
-	// Accept T&Cs
-	await page.waitForSelector("#cbAcceptCGU");
-	await page.click("#cbAcceptCGU");
-	await page.click("#btnOKCGU");
-
-	// // Get rid of overlay
-	await page.click("body");
-
-	// await page.waitFor(5000);
-
-	// // Wait for the results page to load and display the results.
-	// const resultsSelector = "li.li_news_text";
-	// await page.waitForSelector(resultsSelector);
-
-	// // Extract the results from the page.
-	// const stories = await page.evaluate(resultsSelector => {
-	// 	const s = Array.from(document.querySelectorAll(resultsSelector));
-	// 	return s.map(story => {
-	// 		return {
-	// 			headline: story
-	// 				.getElementsByClass("doc-title")[0]
-	// 				.innerHTML.trim()
-	// 		};
-	// 	});
-	// }, resultsSelector);
-	// console.log(links);
-	// console.log("All done");
-	await browser.close();
+		// // Get rid of overlay
+		await page.click("body");
+	} catch(err) {
+		console.error(err);
+		await browser.close();
+		console.timeEnd("AFP update");
+	}
 })();
